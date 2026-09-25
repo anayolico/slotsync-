3dc7efbda49e6926c8fd856d808fb059374c3e37
anayolico <acnwa1234@gmail.com>
2026-09-25 14:08:16 +0100

    gg

--- server/app/main.py
+++ server/app/main.py
@@ -1,6 +1,8 @@
+import os
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from app.api.v1.api import api_router
 from app.core.config import settings
 from app.core.database import init_db
